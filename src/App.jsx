6620ed2d76a9cb458ab52a5fc3c16b67687b6720
unknown <davarayud@gmail.com>
2024-04-15 14:44:59 -0300

    add excercise 5.1

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const userLog = await loginService.login({ username, password })
+      setUser(userLog)
+      setUsername('')
+      setPassword('')
+    } catch {
+      console.log('Wrong credentials')
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h1>Log in to application</h1>
+        <form onSubmit={handleLogin}>
+          <div>
+            Username: <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            Password: <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h2>Blogs</h2>
+      <h3>{user.name} logged in</h3>      
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
